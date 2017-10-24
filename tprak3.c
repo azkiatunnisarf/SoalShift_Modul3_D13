04e19777a8ecea727e80dbec49ea7cd323c58437
azkiatunnisarf <azisarf9@gmail.com>
2017-10-25 00:45:43 +0700

    shift3

---
+++ tprak3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+pthread_t tid[4];
+
+void* aidiil(void *arg){
+unsigned long i=0;
+pthread_id = pthread_self();
+int status;
+if(pthread_equal(id,tid[0])){
+system(");
+}
+}
+else if(pthread_equalIid,tid[1])){
+}
+else if(pthread_equal(id,tid[1])){
+
+}
+else if(pthread_equal(id,tid[2])){
+}
+if(

fa5cf1cad053402cb44545497624b04e78f8170b
azkiatunnisarf <azisarf9@gmail.com>
2017-10-25 16:09:35 +0700

    terbaru

---
+++ tprak4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+pthread_t tid[12];
+
+void* faktorial(void*arg){
+unsigned long i=0;
+pthread_t id=pthread_self();
+if(pthread_equal(id,tid[0])){
+int i,a,bil,n[13];
+long int hasil;
+printf("./faktorial  "); scanf("%d",&n[i]);
+
+for(a=0;a<n[i];a++){
+	hasil=1;
+	for(bil=n[i];bil>=1;bil--) hasil=hasil*bil);
+	printf("%d",&hasil);
+	}
+return NULL;
+}

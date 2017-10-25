429a395852c866f89e3a5676a3a4eca69df2da1a
azkiatunnisarf <azisarf9@gmail.com>
2017-10-25 08:13:53 +0700

    shitf3_rev

--- tprak3.c
+++ tprak3.c
@@ -9,18 +9,25 @@ pthread_t tid[4];
 void* aidiil(void *arg){
 unsigned long i=0;
 pthread_id = pthread_self();
-int status;
-if(pthread_equal(id,tid[0])){
-system(");
-}
-}
-else if(pthread_equalIid,tid[1])){
-}
-else if(pthread_equal(id,tid[1])){
+int lohan=100,kepiting=100;
 
-}
+if(pthread_equal(id,tid[0])){
+	lohan-=15;
+	sleep(10);
+	}
+else if(pthread_equalIid,tid[1])){
+	kepiting-=10;
+	sleep(20);
+	}
 else if(pthread_equal(id,tid[2])){
-}
-if(
-}
+	lohan+=10;
+	sleep(2);
+	}
+else if(pthread_equal(id,tid[2])){
+	kepiting+=10;
+	sleep(2);
+	}
+if((lohan<=0 || kepiting<=0) || (lohan>100 || kepiting>100))
+	break;
+return NULL;
 }

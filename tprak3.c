025ff856040196538f87ceb3a865d4e9c90f9004
azkiatunnisarf <azisarf9@gmail.com>
2017-10-25 08:29:23 +0700

    shift3_rev

--- tprak3.c
+++ tprak3.c
@@ -31,3 +31,22 @@ if((lohan<=0 || kepiting<=0) || (lohan>100 || kepiting>100))
 	break;
 return NULL;
 }
+int main(void){
+int i=0,err;
+char command[30],argument[30];
+
+while(1){
+scanf("%s",command); printf("%s",command);
+
+err=pthread_create(&(tid[0]),NULL,&aidiil,NULL);
+err=pthread_create(&(tid[1]),NULL,&aidiil,NULL);
+
+if(strcmp(command,"beri makan lohan")==0){
+	err=pthread_create(&(tid[2]),NULL,&aidiil, (void*)argument);
+	}
+else if(strcmp(command,"beri makan kepiting")==0){
+	err=pthread_create(&(tid[3],NULL,&aidiil,(void*)argument);
+	}
+}
+return 0;
+}
